9022a4682c2ab6d077049eb388fc2c94d3be654a
Anton <anton.grsv>
2019-06-11 00:58:55 +0300

    Третий коммит

--- test.js
+++ test.js
@@ -1,9 +1,29 @@
-var IceCreamMinus = 10;
+/*
+//Условия
+//Стандартный "if" и "else" операторы 
+    var number = 10;
+    var Jopa = false;
+    if (number == 10){
+        number++;
+        Jopa = "true";
+        console.log(Jopa);
+        console.log(number);
+    }else if(number==11) {
+    console.log("Pizdec");
+    }
+ //Тернарный оператор
+    (number == 10)?console.log("Ravenstvo"):console.log("Neravenstvo");
+ //"switch" оператор
+    switch(number){
+        case number == 10:
+            console.log("10");
+            break;
+        case number == 11:
+            console.log("WoW");
+            break;
+            default: console.log ("Nu LoL");
+            
+    }
+*/
 
-while (IceCreamMinus>0){
-IceCreamMinus = IceCreamMinus - 1;
-console.log(IceCreamMinus);
-}
-if(IceCreamMinus == 0){
-console.log("Done!");
-}
+
