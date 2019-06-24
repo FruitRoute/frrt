53f5dfaf33b0225289500708519a3024076a0229
Anton <anton.grsv>
2019-06-25 00:59:04 +0300

    Я всё вернул

--- test.js
+++ test.js
@@ -1,4 +1,67 @@
-function NameFunction (text){
+/*
+//Условия
+//Стандартный "if" и "else" операторы 
+    var number = 10;
+    var Jopa = false;
+    if (number == 10){
+        number++;
+        Jopa = "true";
+        console.log(Jopa);
+        console.log(number);
+    }else if(number==11) {
+    console.log("Pizdec");
+    }
+ //Тернарный оператор
+    (number == 10)?console.log("Ravenstvo"):console.log("Neravenstvo");
+ //"switch" оператор
+    switch(number){
+        case number == 10:
+            console.log("10");
+            break;
+        case number == 11:
+            console.log("WoW");
+            break;
+            default: console.log ("Nu LoL");
+            
+    }
+*/
 
-    alert(text);
+//Циклы 
+//Цикл "while"
+var JoJo = 10;
+
+while (JoJo < 20){
+    JoJo = JoJo +1;
+    console.log(JoJo);
+    if(JoJo==20){
+        console.log("Done!");
+    }
 }
+
+//"Do" цикл
+do{
+    JoJo = JoJo +1;
+    console.log(JoJo);
+    if(JoJo==20){
+        console.log("Done!");
+    }
+}
+while(JoJo<20);
+
+for(JoJo;JoJo<20; JoJo++){
+    console.log(JoJo);
+}
+
+function NewBalance (a,b){
+    return (a+b);
+}
+NewBalance(2,3);*/
+
+while(JoJo>5){
+    JoJo=JoJo-1;
+    console.log("More!");
+        if(JoJo>5){
+        console.log("Done!");
+        }
+}
+ 
